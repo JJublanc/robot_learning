79d798da78786730c7131409de8d9738f2d89906
Johan <johan.jublanc@gmail.com>
2018-12-21 13:02:54 +0100

    Debug pour lancement 1000 itérations

--- Q_learning.py
+++ Q_learning.py
@@ -132,9 +132,9 @@ def action_choisie(epsilon, s, Q):
     if (s in list(Q.s)) & (rand == 1):
         if max(Q[Q.s == s].q) > 0:
             max_q = max(Q[Q.s == s].q)
-            a = random.choice(Q.loc[(Q.s == s) & (Q.q == max_q), :].a)
+            a = random.choice(Q.loc[(Q.s == s) & (Q.q == max_q), :].reset_index().a)
             # TODO régler le probleme sur la ligne ci-dessous -> on tente
-            # TODO sélectionner les ligne avec loc
+            # TODO sélectionner les lignes avec loc
             # File "pandas\_libs\index.pyx", line 177, in pandas._libs.index.IndexEngine._get_loc_duplicates
             # KeyError: 1
         else:
